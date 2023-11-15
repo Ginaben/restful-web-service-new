33791aca21fa3b5b768540234e87badbcf41aa44
Ginaben <iamgood147@gmail.com>
2023-11-16 00:01:40 +0900

    first commit

---
+++ src/main/java/com/example/restfulwebservicenew/RestfulWebServiceNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulwebservicenew/RestfulWebServiceNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

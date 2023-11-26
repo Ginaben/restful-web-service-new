3193172538aaf55430dfd4c4a9981a714fd052e6
Ginaben <iamgood147@gmail.com>
2023-11-27 00:12:51 +0900

    유효성 체크를 위한 Validation API 사용

--- src/main/java/com/example/restfulwebservicenew/user/User.java
+++ src/main/java/com/example/restfulwebservicenew/user/User.java
@@ -3,12 +3,16 @@ package com.example.restfulwebservicenew.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor //모든 필드에 대해 파라미터로 갖는 생성자
 public class User{
     private Integer id;
+    @Size(min=2, message = "Name은 두글자 이상 입력해주세요.")
     private String name;
+    @Past
     private Date joinDate;
 }

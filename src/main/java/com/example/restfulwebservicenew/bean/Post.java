360b71acfc4af2c96a177e3ec4ffb9cd5b21bc6d
Ginaben <iamgood147@gmail.com>
2024-01-02 23:55:51 +0900

    게시물 관리를 위한 Post Entity 추가와 초기 데이터 생성

---
+++ src/main/java/com/example/restfulwebservicenew/bean/Post.java
@@ -0,0 +1,23 @@
+package com.example.restfulwebservicenew.bean;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor //전체 생성자
+@NoArgsConstructor // 디폴트 생성자
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+}

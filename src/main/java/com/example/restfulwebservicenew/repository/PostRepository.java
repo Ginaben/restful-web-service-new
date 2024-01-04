14a3e76da8d3f013c9d37b5bb5f27df17f0b58ae
Ginaben <iamgood147@gmail.com>
2024-01-04 19:41:51 +0900

    JPA를 이용한 새 게시물 추가 - HTTP POST Method

---
+++ src/main/java/com/example/restfulwebservicenew/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.restfulwebservicenew.repository;
+
+import com.example.restfulwebservicenew.bean.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+}
--- src/main/java/com/example/restfulwebservicenew/repository/UserRepository.java
+++ src/main/java/com/example/restfulwebservicenew/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.restfulwebservicenew.repository;
 
 import com.example.restfulwebservicenew.bean.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
